dac748a3217ac064af50c957f7f3f839f3626e95
paolo <1192706763@qq.com>
2018-10-15 08:33:25 +0800

    init

---
+++ miniprogram/pages/listenMusic/listenMusic.js
@@ -0,0 +1,89 @@
+// pages/listenMusic/listenMusic.js
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    audioNames:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.cloud.callFunction({
+      name:"getAudioList",
+      data:{
+        offset:0,
+        pageSize:10
+      }
+    }).then(res=>{
+      console.log(res)
+      this.setData({
+        audioNames:res.result.data
+      })
+    }).catch(err=>{
+      console.log(err)
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  playAudio(e){
+    let audioname = e.currentTarget.dataset.audioname;
+    let audioContext = wx.createInnerAudioContext();
+    audioContext.src = app.globalData.audioBasePath+ "钱半仙 - 梁祝（钢琴版）.mp3";
+    audioContext.play()
+
+    // console.log( new AudioContext())
+  }
+})
