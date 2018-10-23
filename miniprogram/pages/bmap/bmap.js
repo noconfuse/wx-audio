4c5dab43900604a4f1b1a455c31a54b9ac6b9b95
paolo <1192706763@qq.com>
2018-10-23 08:38:40 +0800

    dev

--- miniprogram/pages/bmap/bmap.js
+++ miniprogram/pages/bmap/bmap.js
@@ -1,11 +1,42 @@
 const bmap = require('../../lib/bmap-wx.min.js')
 Page({
   data: {
-    sugData: []
+    sugData: [],
+    initLatitude:0,
+    initLongitude:0,
+    markers:{
+      id:0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      iconPath:'../../images/yellpwduck.png'
+    }
   },
   onLoad: function (options) {
-    console.log(bmap)
+    // wx.request({
+    //   url:'http://192.168.1.3:3001/map/transit',
+    //   method:'post',
+    //   data:{
 
+    //   },
+    //   success:(result)=>{
+    //     console.log(result)
+    //   }
+    // })
+    var that = this;
+    wx.getLocation({
+      type:'gcj02',
+      success: function(res) {
+        console.log(res)
+        that.setData({
+          initLatitude:res.latitude,
+          initLongitude:res.longitude,
+          markers:{
+            longitude:res.longitude,
+            latitude:res.latitude
+          }
+        })
+      },
+    })
   },
   bindkeyinput:function(e){
     var that = this;
